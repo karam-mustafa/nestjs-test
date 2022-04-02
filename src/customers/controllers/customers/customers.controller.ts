87671feab9f9babf90602cc600ed1fb52fa41266
karam  mustafa <karam2mustafa@gmail.com>
2022-04-02 22:14:25 +0300

    wip

--- src/customers/controllers/customers/customers.controller.ts
+++ src/customers/controllers/customers/customers.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+
+  @Get('')
+  getCustomer(){
+    return {
+      email:'karam2mustafa@gmail.com',
+      name:'karam mustafa',
+    }
+  }
+
+}
